48cd36af0b15a1304a0f6f1e7d06459d48d8bf7b
locutus3009 <nikolay.nerovniy@gmail.com>
2021-02-25 10:50:59 +0300

    Example test commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# This is a sample Python script.
+def sum_and_duplication(a, b):
+    return (a + b) * 2
+
+
+if __name__ == '__main__':
+    var_a = int(input('Input first number: '))
+    var_b = int(input('Input second number: '))
+    print('Sum and duplication: ' + str(sum_and_duplication(var_a, var_b)))
