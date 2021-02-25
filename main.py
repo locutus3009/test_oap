8cd672f5ce7171dc987805f3bdb9e530841fa675
locutus3009 <nikolay.nerovniy@gmail.com>
2021-02-25 10:53:54 +0300

    New changes

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ def sum_and_duplication(a, b):
     return (a + b) * 2
 
 
+def print_result(data):
+    print('Result is: ' + str(data))
+
+
 if __name__ == '__main__':
     var_a = int(input('Input first number: '))
     var_b = int(input('Input second number: '))
-    print('Sum and duplication: ' + str(sum_and_duplication(var_a, var_b)))
+    temp = sum_and_duplication(var_a, var_b)
+    print_result(temp)
